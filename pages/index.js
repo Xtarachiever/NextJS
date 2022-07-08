be5149abc7913346d7156929f4e288c10703fb79
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-08 22:59:44 +0100

    refactoring codes

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      folder1
-      vhfhfh
+      
     </div>
   )
 }
