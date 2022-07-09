a07a2f70536e8c5b590d238267bbec3f2a2efdb8
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-09 09:58:22 +0100

    Added Link to Navbar

--- fixed_pages/navbar.js
+++ fixed_pages/navbar.js
@@ -1,13 +1,14 @@
+import Link from 'next/link';
 import styles from '../styles/Layout.module.css';
 const Navbar = () =>{
     return(
         <div className={styles.nav}>
             <div className={styles.container}>
-                <h4>Collections</h4>
+                <Link href="/"><a>Collections</a></Link>
                 <div className={styles.nav_content}>
-                  <p>Explore Users</p>
-                  <p>About</p>
-                  <p>Contact Us</p>
+                  <Link href="/users"><a>Explore Users</a></Link>
+                  <Link href="/about"><a>About</a></Link>
+                  <Link href="/contact"><a>Contact Us</a></Link>
                 </div>
             </div>
         </div>
