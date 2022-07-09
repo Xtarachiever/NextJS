6af5a1822a5754da4cad9df3b0a048a9c8d8ab4b
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-09 01:11:16 +0100

    home page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,19 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.section}> 
+        <div className={styles.content}>
+          <h1>We have the best collection of unique anime movies</h1>
+          <p>There are various animes in this site to select from. This site shows the lists of user that watches anime and are officially a user.</p>
+          <div className={styles.button_wrapper}>
+            <button type='button' className={styles.button}>Lets Explore</button>
+            <button type='button' className={styles.button}>Anime Images</button>
+          </div>
+        </div>
+        <div className={styles.image_wrapper}>
+          <Image src="/anime_cover.jpeg" height={300} width={250} alt="anime_cover"/>
+        </div>
+      </div>
     </div>
   )
 }
