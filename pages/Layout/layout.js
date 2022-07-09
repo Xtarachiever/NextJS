8970924dcc173b8b59445b2baabc524fbbff1529
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-09 02:32:25 +0100

    created the footer

--- pages/Layout/layout.js
+++ pages/Layout/layout.js
@@ -1,11 +1,12 @@
 import Navbar from "../fixed_pages/navbar";
+import Footer from "../fixed_pages/footer";
 
 const Layout = ({children}) =>{
     return(
         <div>
             <Navbar/>
             {children}
-            
+            <Footer/>
         </div>
     )
 }
---
+++ pages/fixed_pages/footer.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Layout.module.css';
+const Footer = () =>{
+    return(
+        <div className={styles.footer}>
+            <p>Copyright 2022</p>
+        </div>
+    )
+}
+export default Footer;
