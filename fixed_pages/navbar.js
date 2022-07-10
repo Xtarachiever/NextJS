897245237f4c0408ef771693cf70420b4dbf89ad
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-10 19:22:59 +0100

    Added a responsive navbar using tailwindcss

--- fixed_pages/navbar.js
+++ fixed_pages/navbar.js
@@ -1,33 +1,43 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from '../styles/Layout.module.css';
 const Navbar = () =>{
+  const [active, setActive] = useState(false)
+  const handleClick = () =>{
+    setActive(!active)
+  }
     return(
-        <div className={styles.nav}>
+        <nav className={`${styles.nav}`}>
             <div className={styles.container}>
+              <div className="flex flex-col">
                 <Link href="/"><a>Collections</a></Link>
-                <div className={`hidden lg:inline-flex ${styles.nav_content}`}>
-                  <Link href="/users"><a>Explore Users</a></Link>
-                  <Link href="/about"><a>About</a></Link>
-                  <Link href="/contact"><a>Contact Us</a></Link>
-                </div>
-				<button className=' inline-flex p-3 hover:bg-green-600 rounded sm:hidden text-black ml-auto hover:text-white outline-none'>
-          <svg
-            className='w-6 h-6'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M4 6h16M4 12h16M4 18h16'
-            />
-          </svg>
-        </button>
+                    <div className={`${active ? '' : 'hidden'} w-full lg:inline-flex lg:w-auto lg:flex-grow `}>
+                      <div className='lg:ml-auto lg:inline-flex lg:w-auto w-full flex flex-col items-start'>
+                        <Link href="/users"><a className='w-full pt-2'>Explore Users</a></Link>
+                        <Link href="/about"><a className='w-full pt-2'>About</a></Link>
+                        <Link href="/contact"><a className='w-full py-2'>Contact Us</a></Link>
+                      </div>
+                    </div>
+              </div>
+                <button className=' inline-flex hover:bg-blue-600 rounded 
+                sm:hidden text-black ml-auto hover:text-white outline-none' onClick={handleClick}>
+                  <svg
+                    className='w-6 h-6'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth={2}
+                      d='M4 6h16M4 12h16M4 18h16'
+                    />
+                  </svg>
+                </button>
             </div>
-        </div>
+        </nav>
     )
 }
 export default Navbar
