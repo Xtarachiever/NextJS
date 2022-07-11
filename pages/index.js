63f1494657050729d115019aea74559aca410b74
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-11 09:43:59 +0100

    added styles to the home page

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.section}> 
-        <div className={styles.content}>
-          <h1>We have the best collection of unique anime movies</h1>
-          <p>There are various animes in this site to select from. This site shows the lists of user that watches anime and are officially a user.</p>
-          <div className={styles.button_wrapper}>
-            <button type='button' className={styles.button}>Lets Explore</button>
-            <button type='button' className={styles.button}>Anime Images</button>
+      <div className={`flex flex-col ${styles.section}`}> 
+        <div className={`order-2 sm:order-none ${styles.content}`}>
+          <h1 className='text-center sm:text-left'>We have the best collection of unique anime movies</h1>
+          <p className='text-center sm:text-left'>There are various animes in this site to select from. This site shows the lists of user that watches anime and are officially a user.</p>
+          <div className={`flex flex-col mb-3 ${styles.button_wrapper}`}>
+            <button type='button' className={` ${styles.button}`}>Lets Explore</button>
+            <button type='button' className={`${styles.button}`}>Anime Images</button>
           </div>
         </div>
         <div className={styles.image_wrapper}>
